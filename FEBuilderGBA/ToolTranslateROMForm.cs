d24b9386149a293614b1fb2f7f550d02cd485fe0
LPFan12 <81183687+LPFan12@users.noreply.github.com>
2025-04-19 23:34:29 +0200

    Update ToolTranslateROMForm.cs

--- FEBuilderGBA/ToolTranslateROMForm.cs
+++ FEBuilderGBA/ToolTranslateROMForm.cs
@@ -422,7 +422,58 @@ namespace FEBuilderGBA
                     LabelTranslateToROMFilename.Text = R._("無改造 FE8J");
                 }
             }
+            else if (Program.ROM.RomInfo.version == 531)
+            {
+                if (Program.ROM.RomInfo.is_multibyte)
+                {
+                    LabelSimpleTranslateFromROMFilename.Text = R._("無改造 FE8J");
+                    LabelSimpleTranslateToROMFilename.Text = R._("無改造 FE8U");
+                    LabelTranslateFormROMFilename.Text = R._("無改造 FE8J");
+                    LabelTranslateToROMFilename.Text = R._("無改造 FE8U");
+                }
+                else
+                {
+                    LabelSimpleTranslateFromROMFilename.Text = R._("無改造 FE8U");
+                    LabelSimpleTranslateToROMFilename.Text = R._("無改造 FE8J");
+                    LabelTranslateFormROMFilename.Text = R._("無改造 FE8U");
+                    LabelTranslateToROMFilename.Text = R._("無改造 FE8J");
+                }
+            }
             else if (Program.ROM.RomInfo.version == 7)
+            {
+                if (Program.ROM.RomInfo.is_multibyte)
+                {
+                    LabelSimpleTranslateFromROMFilename.Text = R._("無改造 FE7U");
+                    LabelSimpleTranslateToROMFilename.Text = R._("無改造 FE7J");
+                    LabelTranslateFormROMFilename.Text = R._("無改造 FE7U");
+                    LabelTranslateToROMFilename.Text = R._("無改造 FE7J");
+                }
+                else
+                {
+                    LabelSimpleTranslateFromROMFilename.Text = R._("無改造 FE7J");
+                    LabelSimpleTranslateToROMFilename.Text = R._("無改造 FE7U");
+                    LabelTranslateFormROMFilename.Text = R._("無改造 FE7J");
+                    LabelTranslateToROMFilename.Text = R._("無改造 FE7U");
+                }
+            }
+            else if (Program.ROM.RomInfo.version == 206)
+            {
+                if (Program.ROM.RomInfo.is_multibyte)
+                {
+                    LabelSimpleTranslateFromROMFilename.Text = R._("無改造 FE7U");
+                    LabelSimpleTranslateToROMFilename.Text = R._("無改造 FE7J");
+                    LabelTranslateFormROMFilename.Text = R._("無改造 FE7U");
+                    LabelTranslateToROMFilename.Text = R._("無改造 FE7J");
+                }
+                else
+                {
+                    LabelSimpleTranslateFromROMFilename.Text = R._("無改造 FE7J");
+                    LabelSimpleTranslateToROMFilename.Text = R._("無改造 FE7U");
+                    LabelTranslateFormROMFilename.Text = R._("無改造 FE7J");
+                    LabelTranslateToROMFilename.Text = R._("無改造 FE7U");
+                }
+            }
+            else if (Program.ROM.RomInfo.version == 209)
             {
                 if (Program.ROM.RomInfo.is_multibyte)
                 {
