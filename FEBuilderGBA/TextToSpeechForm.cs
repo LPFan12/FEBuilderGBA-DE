c21830121e43f40c04a21024ccf0111d826000f3
LPFan12 <81183687+LPFan12@users.noreply.github.com>
2025-04-22 12:05:13 +0200

    Update TextToSpeechForm.cs

--- FEBuilderGBA/TextToSpeechForm.cs
+++ FEBuilderGBA/TextToSpeechForm.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
-using SpeechLib; //for ver11
+using System.Speech.Synthesis;
 
 namespace FEBuilderGBA
 {
