f67fd22a2befe093ec6377ff856126de7438a259
LPFan12 <81183687+LPFan12@users.noreply.github.com>
2025-05-08 23:58:09 +0200

    Update EventCondForm.cs
    Test

--- FEBuilderGBA/EventCondForm.cs
+++ FEBuilderGBA/EventCondForm.cs
@@ -163,12 +163,17 @@ namespace FEBuilderGBA
                 OBJECT_N06_L_10_COMBO.Items.Add(R._("10=民家"));
                 OBJECT_N06_L_10_COMBO.AddIcon(0x10, ImageSystemIconForm.Village()); //10=民家
 
+                OBJECT_N07_L_10_COMBO.Items.Add(R._("14=宝箱"));
                 OBJECT_N07_L_10_COMBO.AddIcon(0x14, ImageSystemIconForm.Chest()); //14=宝箱
 
+                OBJECT_N08_L_10_COMBO.Items.Add(R._("12=扉"));
                 OBJECT_N08_L_10_COMBO.AddIcon(0x12, ImageSystemIconForm.Door()); //12=扉
 
+                OBJECT_N0A_L_10_COMBO.Items.Add(R._("16=武器屋"));
                 OBJECT_N0A_L_10_COMBO.AddIcon(0x16, ImageSystemIconForm.Armory()); //16=武器屋
+                OBJECT_N0A_L_10_COMBO.Items.Add(R._("17=道具屋"));
                 OBJECT_N0A_L_10_COMBO.AddIcon(0x17, ImageSystemIconForm.Vendor()); //17=道具屋
+                OBJECT_N0A_L_10_COMBO.Items.Add(R._("18=秘密の店"));
                 OBJECT_N0A_L_10_COMBO.AddIcon(0x18, ImageSystemIconForm.SecretShop()); //18=秘密の店
 
                 if (PatchUtil.SearchSkillSystem() == PatchUtil.skill_system_enum.SkillSystem)
@@ -200,14 +205,18 @@ namespace FEBuilderGBA
                 OBJECT_N06_L_10_COMBO.Items.Add(R._("0F=民家"));
                 OBJECT_N06_L_10_COMBO.AddIcon(0x0F, ImageSystemIconForm.Village()); //0F=民家
 
-                OBJECT_N0G_L_10_COMBO.AddIcon(0x13, ImageSystemIconForm.Chest()); //13=宝箱
+                OBJECT_N07_L_10_COMBO.Items.Add(R._("0=--"));
+                OBJECT_N07_L_10_COMBO.AddIcon(0x13, ImageSystemIconForm.Chest()); //13=宝箱
 
-                OBJECT_N0H_L_10_COMBO.Items.Add(R._("11=扉"));
-                OBJECT_N0H_L_10_COMBO.AddIcon(0x11, ImageSystemIconForm.Door()); //11=扉
+                OBJECT_N08_L_10_COMBO.Items.Add(R._("11=扉"));
+                OBJECT_N08_L_10_COMBO.AddIcon(0x11, ImageSystemIconForm.Door()); //11=扉
 
-                OBJECT_N0I_L_10_COMBO.AddIcon(0x14, ImageSystemIconForm.Armory()); //16=武器屋
-                OBJECT_N0I_L_10_COMBO.AddIcon(0x15, ImageSystemIconForm.Vendor()); //Muss getestet werden
-                OBJECT_N0I_L_10_COMBO.AddIcon(0x16, ImageSystemIconForm.SecretShop()); //Muss getestet werden
+                OBJECT_N0A_L_10_COMBO.Items.Add(R._("14=武器屋"));
+                OBJECT_N0A_L_10_COMBO.AddIcon(0x14, ImageSystemIconForm.Armory()); //14=武器屋
+                OBJECT_N0A_L_10_COMBO.Items.Add(R._("15=道具屋"));
+                OBJECT_N0A_L_10_COMBO.AddIcon(0x15, ImageSystemIconForm.Vendor()); //Muss getestet werden
+                OBJECT_N0A_L_10_COMBO.Items.Add(R._("16=秘密の店"));
+                OBJECT_N0A_L_10_COMBO.AddIcon(0x16, ImageSystemIconForm.SecretShop()); //Muss getestet werden
 
                 return;
             }
